c2b13fdf08c7082bb401a764e0f0a9ec51e67a29
Maxi Rubio <maxirubio92@gmail.com>
2022-09-01 14:57:15 -0300

    libreria

--- codigo.js
+++ codigo.js
@@ -20,6 +20,11 @@ const botonBorrar = document.getElementById("btnBorrar")
 const divHsEstudio = document.getElementById("divHsEstudio")
 
 form.addEventListener('submit', (e) => {
+	Swal.fire({
+		icon: 'success',
+		title: 'ENVIADO'
+	  })
+	  
     e.preventDefault()
     const datosForm = new FormData(e.target)
 
@@ -41,20 +46,37 @@ botonHsEstudio.addEventListener('click', () => {
         divHsEstudio.innerHTML += `
 			<ul id=horaEst${indice}>
 				<li>El dia ${estudio.dia}, estudie ${estudio.materia}, durante ${estudio.horas}</li>
-				<button class="btn2">Borrar</button>
+				<button class="btn2 botonBorrar">Borrar</button>
 			</ul>
         
         `
     })
+
     horasEst.forEach((estudio, indice) => {
         const borrarElemnto = document.getElementById(`horaEst${indice}`)
 
 		borrarElemnto.children[1].addEventListener('click', () => {
+			Toastify({
+				text: `Día ${estudio.dia} ELIMINADO`,
+				duration: 3000,
+				// destination: "https://github.com/apvarun/toastify-js",
+				// newWindow: true,
+				close: false,
+				gravity: "top", // `top` or `bottom`
+				position: "right", // `left`, `center` or `right`
+				stopOnFocus: true, // Prevents dismissing of toast on hover
+				style: {
+				  background: "linear-gradient(to right, #350b5c, #000000)",
+				  color: "#ffffff"
+				},
+				onClick: function(){} // Callback after click
+			  }).showToast();
+
 			borrarElemnto.remove()
 		 	HsEstudio.splice(indice, 1)
 			localStorage.setItem("HsEstudio", JSON.stringify(HsEstudio))
-			console.log(`${estudio.materia} ELIMINADA`)
 		})
 		
     })
 })
+
